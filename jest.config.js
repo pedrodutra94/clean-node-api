8e6a64f70e33d0ed0ea3c4813ddcf68750cce4e3
Pedro Dutra <pedrodutrafoz@gmail.com>
2021-04-06 04:03:22 -0300

    chore: ignore files in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*protocols.ts'
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    'index.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {

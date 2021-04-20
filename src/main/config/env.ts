bd4e31e1175d1133bde1c815189b139f5a1fac8e
Pedro Dutra <pedrodutrafoz@gmail.com>
2021-04-20 01:46:24 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://pedutra94:cantillo94@cluster0.k5in3.mongodb.net/sample_analytics?retryWrites=true&w=majority',
+  port: process.env.PORT || 5050
+}

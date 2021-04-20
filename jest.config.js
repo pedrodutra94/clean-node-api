d0834c182ea1b4d1484191e33f9d8ad148cf3c38
Pedro Dutra <pedrodutrafoz@gmail.com>
2021-04-20 02:35:27 -0300

    chore: ignore main layer from test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/**/*protocols.ts'
+    '!<rootDir>/src/**/*protocols.ts',
+    '!<rootDir>/src/main/**'
   ],
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',

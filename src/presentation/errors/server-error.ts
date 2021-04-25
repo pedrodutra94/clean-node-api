e5e1fe0840027dcf378e91c4db9ec22c42edf981
Pedro Dutra <pedrodutrafoz@gmail.com>
2021-04-24 23:17:36 -0300

    refactor: make server error return the error stack

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,6 +1,7 @@
 export class ServerError extends Error {
-  constructor () {
+  constructor (stack) {
     super('Internal server error')
     this.name = 'ServerError'
+    this.stack = stack
   }
 }
